939f3f22addd6672a559cf2440b8d73d79913fbd
hellolinuxworld <hellolinux@qq.com>
2018-07-15 12:21:58 +0800

    import code snippets about qml

---
+++ fallingcubes/fallingcubes.pro
@@ -0,0 +1,6 @@
+TEMPLATE = app
+QT += widgets
+QT += quick
+TARGET = fallingcubes
+SOURCES += main.cpp
+CONFIG += qt3dquick
---
+++ fallingcubes/main.cpp
@@ -0,0 +1,19 @@
+#include <QApplication>
+//#include <QDeclarativeView3D>
+#include <qdeclarativeview3d.h>
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    QDeclarativeView3D view;
+    view.setSource(QUrl::fromLocalFile(QLatin1String("fallingcubes.qml")));
+
+    if (QApplication::arguments().contains(QLatin1String("-maximize")))
+        view.showMaximized();
+    else if (QApplication::arguments().contains(QLatin1String("-fullscreen")))
+        view.showFullScreen();
+    else
+        view.show();
+
+    return app.exec();
+}
